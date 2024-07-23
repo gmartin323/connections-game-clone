3a4715ccea79a9395daa3efab0c2fbea70d61124
gmartin323 <116142265+gmartin323@users.noreply.github.com>
2024-07-23 16:25:59 +0700

    Dsiplay difficulty and star rating on GameSelectTiles

--- src/components/GameSelectTile.jsx
+++ src/components/GameSelectTile.jsx
@@ -1,17 +1,42 @@
 import React from 'react'
 
-
 export default function GameSelectTile( { game } ) {
 
   const {id, Title, Author, Difficulty} = game
 
+  let starRatingEl
+
+  function getStarRatingEl(num) {
+    const filledSkulllIcon = <i class="fa-solid fa-skull" style={{color: 'coral', padding: '1px'}}></i>
+    const emptySkullIcon = <i class="fa-solid fa-skull" style={{color: '#e3d8d3', padding: '1px'}}></i>
+    let filledStarEl = []
+    for (let i=0; i < num; i++ ) {
+      filledStarEl.push(filledSkulllIcon)
+    }
+    let emptyStarsEl = []
+    for (let i=0; i < 5 - num; i++ ) {
+      filledStarEl.push(emptySkullIcon)
+    }
+
+    starRatingEl = <p className="rating">{filledStarEl}{emptyStarsEl}</p>
+    return starRatingEl
+  }
+
+  const randomStarRating = Math.random()* (5 - 1) + 1
+
+  console.log(randomStarRating)
+
   return (
     <div className="game-selection-tile" >
       <img src="src\images\board-100x100.svg" alt="template of 4x4 game board" />
-      {/* <div className="game-selection-tile-info">
-        <p>{Difficulty}/5</p>
-        <p>by {Author}</p>
-      </div> */}
+      <div className="game-selection-tile-info">
+        {getStarRatingEl(Difficulty)}
+        <p>
+          <i className="fa-solid fa-star"></i>
+          <span className='star-rating-num'>{randomStarRating.toFixed(1)}</span>
+        </p>
+        {/* <p>by {Author}</p> */}
+      </div>
       <h3 className="game-selection-tile-title">{Title}</h3>
     </div>
   )
